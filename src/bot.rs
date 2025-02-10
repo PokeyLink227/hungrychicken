6a74c1880ce3f1a4d62a0e2b3eba0838eb73d06d
Thomas Rice <thomasrice7@yahoo.com>
2025-02-09 22:56:09 -0500

    added better filtering

--- src/bot.rs
+++ src/bot.rs
@@ -9,32 +9,122 @@ use rand::Rng;
 use regex::{Regex, RegexBuilder};
 use std::{
     cell::LazyCell,
+    ops::Sub,
     str::FromStr,
     time::{Duration, Instant},
 };
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Rule {
+    name: String,
     filters: Vec<Filter>,
     action: BotAction,
 }
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
+pub enum Op {
+    Eq,
+    Lt,
+    Gt,
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
+pub enum Field {
+    Report,
+    Depart,
+    Arrive,
+    Block,
+    Credit,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub enum Filter {
-    NumDays(u8, u8),
-    OnDay(Date),
+    TimeDiff(Field, Field, Op, Time),
+    FieldIs(Field, Op, Time),
+    DateIs(Op, Date),
+    IncludeLayover(String),
+    ExcludeLayover(String),
+    NumDays(Op, u8),
+    IsPrem,
+    IncludeId(String),
+}
+
+impl Filter {
+    pub fn eval(&self, trip: &Trip) -> bool {
+        match self {
+            Filter::TimeDiff(lhs, rhs, op, val) => match op {
+                Op::Eq => trip.get(*lhs) - trip.get(*rhs) == *val,
+                Op::Lt => trip.get(*lhs) - trip.get(*rhs) < *val,
+                Op::Gt => trip.get(*lhs) - trip.get(*rhs) > *val,
+            },
+            Filter::FieldIs(field, op, val) => match op {
+                Op::Eq => trip.get(*field) == *val,
+                Op::Lt => trip.get(*field) < *val,
+                Op::Gt => trip.get(*field) > *val,
+            },
+            Filter::DateIs(op, val) => match op {
+                Op::Eq => trip.date == *val,
+                Op::Lt => trip.date < *val,
+                Op::Gt => trip.date > *val,
+            },
+            Filter::IncludeLayover(val) => trip.layovers.contains(val),
+            Filter::ExcludeLayover(val) => !trip.layovers.contains(val),
+            Filter::NumDays(op, val) => match op {
+                Op::Eq => trip.days == *val,
+                Op::Lt => trip.days < *val,
+                Op::Gt => trip.days > *val,
+            },
+            Filter::IsPrem => trip.premium,
+            Filter::IncludeId(val) => trip.id == *val,
+        }
+    }
 }
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum BotAction {
     Alert,
-    AutoPickup,
+    Pickup,
 }
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
 pub struct Time {
     hours: u8,
     minutes: u8,
+}
+
+impl Time {
+    pub fn from_num_str(s: &str) -> Result<Self, ParseTimeError> {
+        Ok(Time {
+            hours: s[0..2].parse().or(Err(ParseTimeError))?,
+            minutes: s[2..3].parse().or(Err(ParseTimeError))?,
+        })
+    }
+}
+
+impl Sub for Time {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        if rhs.minutes > self.minutes {
+            Time {
+                hours: if rhs.hours > self.hours {
+                    self.hours - rhs.hours - 1
+                } else {
+                    12 + self.hours - rhs.hours - 1
+                },
+                minutes: 60 + self.minutes - rhs.minutes,
+            }
+        } else {
+            Time {
+                hours: if rhs.hours > self.hours {
+                    self.hours - rhs.hours
+                } else {
+                    12 + self.hours - rhs.hours
+                },
+                minutes: self.minutes - rhs.minutes,
+            }
+        }
+    }
 }
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
@@ -51,7 +141,7 @@ impl FromStr for Time {
     }
 }
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
 pub struct Date {
     year: u16,
     month: u8,
@@ -99,10 +189,22 @@ pub struct Trip {
     report: Time,
     depart: Time,
     arrive: Time,
-    block: u16,
-    credit: u16,
+    block: Time,
+    credit: Time,
     layovers: Vec<String>,
     premium: bool,
+}
+
+impl Trip {
+    pub fn get(&self, field: Field) -> Time {
+        match field {
+            Field::Report => self.report,
+            Field::Depart => self.depart,
+            Field::Arrive => self.arrive,
+            Field::Block => self.block,
+            Field::Credit => self.credit,
+        }
+    }
 }
 
 pub async fn monitor_opentime() -> Message {
@@ -122,7 +224,15 @@ pub async fn monitor_opentime() -> Message {
     let mut refresh_interval = Duration::from_secs(20);
 
     let rule = Rule {
-        filters: vec![Filter::NumDays(3, 3)],
+        name: "trips after 0700".to_owned(),
+        filters: vec![Filter::FieldIs(
+            Field::Report,
+            Op::Gt,
+            Time {
+                hours: 7,
+                minutes: 0,
+            },
+        )],
         action: BotAction::Alert,
     };
 
@@ -161,8 +271,8 @@ pub async fn monitor_opentime() -> Message {
                     report: rep.parse().unwrap(),
                     depart: dep.parse().unwrap(),
                     arrive: arr.parse().unwrap(),
-                    block: blk.parse().unwrap(),
-                    credit: crd.parse().unwrap(),
+                    block: Time::from_num_str(blk).unwrap(),
+                    credit: Time::from_num_str(crd).unwrap(),
                     layovers: lay.split_whitespace().map(|s| s.to_owned()).collect(),
                     premium: !prem.is_empty(),
                 },
@@ -175,10 +285,7 @@ pub async fn monitor_opentime() -> Message {
             .filter(|t| {
                 rule.filters
                     .iter()
-                    .map(|f| match f {
-                        Filter::NumDays(l, r) => t.days >= *l && t.days <= *r,
-                        Filter::OnDay(d) => t.date == *d,
-                    })
+                    .map(|f| f.eval(t))
                     .fold(true, |a, b| a & b)
             })
             .collect();
@@ -187,17 +294,20 @@ pub async fn monitor_opentime() -> Message {
         filtered_trips.iter().for_each(|t| println!("{}", t.id));
 
         // TEMP: add and submit a trip
-        //add_trip_from_otadd(&mut enigo, "j2B15").await;
+        //add_trip_from_opentime(&mut enigo, "j2B15").await;
+        break;
 
         // sleep for a random ammount of time
         let milis_to_sleep = rand::random_range(800..2000);
         let mut m = 0;
         while m < milis_to_sleep {
             // check if Escape key is pressed
+
             if unsafe { winapi::um::winuser::GetKeyState(27) } & 0x8000u16 as i16 != 0 {
                 return Message::Pause;
             }
             async_std::task::sleep(Duration::from_millis(50)).await;
+            m += 50;
         }
     }
 
@@ -218,18 +328,35 @@ async fn refresh_page(enigo: &mut Enigo, loc: (i32, i32)) {
 }
 
 async fn add_trip_from_otadd(enigo: &mut Enigo, trip_id: &str) {
+    hit_button(enigo, trip_id).await;
+    hit_button(enigo, "it r").await;
+}
+
+async fn add_trip_from_opentime(enigo: &mut Enigo, trip_id: &str) {
+    hit_button(enigo, "submit").await;
+    async_std::task::sleep(Duration::from_millis(2000)).await; // this delay needs to wait until the page has loaded
+    hit_button(enigo, "add").await;
+    //hit_button(enigo, trip_id).await;
+    //hit_button(enigo, "it r").await;
+}
+
+// these durations should be randomized if possible, should total to ~1 sec
+async fn hit_button(enigo: &mut Enigo, button_name: &str) {
     // open quick find bar
     enigo.key(Key::Unicode('/'), Click);
-    async_std::task::sleep(Duration::from_millis(100)).await;
+    async_std::task::sleep(Duration::from_millis(25)).await;
 
-    // type trip id
-    enigo.text(trip_id);
+    // type button name
+    enigo.text(button_name);
+    async_std::task::sleep(Duration::from_millis(25)).await;
 
-    // navigate to add button
+    // navigate to button
     enigo.key(Key::Shift, Press);
     enigo.key(Key::Tab, Click);
     enigo.key(Key::Shift, Release);
+    async_std::task::sleep(Duration::from_millis(50)).await;
 
-    // click add button
+    // click button
     enigo.key(Key::Return, Click);
+    async_std::task::sleep(Duration::from_millis(5)).await;
 }
